f07c74ac2fa89dc2731f98a3528fab0891f257c3
JM.JAHITH <122439457+jahithjm@users.noreply.github.com>
2024-07-14 13:45:01 +0530

    Add files via upload
    All files

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:779237432"><i class="fas fa-phone"></i><span>779237432</span></a>
+         <!-- <a href="tel:1112223333"><i class="fas fa-phone"></i><span></span></a> -->
+         <a href="mailto:jahith418@gmail.com"><i class="fas fa-envelope"></i><span>jahith418@gmail.com</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>MK</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="listings.php"><span>all listings</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="#"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Mr. JM.JAHITH</span> </div>
+
+</footer>
+
+<!-- footer section ends -->
